0f6f57cd5ff749efc17d0fc979f3fb3d9604e3a1
baduraje@isu.edu <baduraje@isu.edu>
2020-08-24 17:43:29 -0600

    made changes to controller.js, index.handlebars,burger.js and added partials folder in views

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -34,7 +34,7 @@ router.get("/", function(req, res) {
     console.log("condition", condition);
   
     burger.update({
-      devoured: req.body.sleepy
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
--- db/schema.sql
+++ db/schema.sql
@@ -1,12 +1,12 @@
 ### Schema
 
-CREATE DATABASE burger_db;
-USE burger_db;
+CREATE DATABASE burgers_db;
+USE burgers_db;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) ,
-    devoured boolean
+    devoured boolean,
 	PRIMARY KEY (id)
 );
